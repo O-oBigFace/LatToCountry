cf5ace493d0d35b36ccdc156174eb75a9531945a
John <jiangh_wu@163.com>
2018-11-14 09:03:57 +0800

    增加了根据地理名称获取位置

--- proxy_provider.py
+++ proxy_provider.py
@@ -1,3 +1,15 @@
+p = {
+    "proxyHost": "proxy.crawlera.com",
+    "proxyPort": "8010",
+    "proxyAuth": "78d4b6f49900465f881f63b4b1de4029:"
+}
+
+proxy_auth = p['proxyHost']
+proxy_host = p['proxyHost']
+proxy_port = p['proxyPort']
+
 
 def get_proxy():
+    # return {"https": "https://{}@{}:{}/".format(proxy_auth, proxy_host, proxy_port),
+    #         "http": "http://{}@{}:{}/".format(proxy_auth, proxy_host, proxy_port)}
     return {'http': 'socks5://127.0.0.1', 'https': 'socks5://127.0.0.1'}
--- spider4country.py
+++ spider4country.py
@@ -3,17 +3,23 @@ import requests
 import proxy_provider
 import os
 
-__HOST = "https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key=AIzaSyDDzBNqMLO96aIRbNh18LqnVeagfJK1-s8"
+__GOOGLE_KEY = "AIzaSyDDzBNqMLO96aIRbNh18LqnVeagfJK1-s8"
+__HOST_lo_la = "https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key=" + __GOOGLE_KEY
+__HOST_loc = "https://maps.googleapis.com/maps/api/geocode/json?address={}&key=" + __GOOGLE_KEY
+
+__MAX_TRY = 6
 
 
-def toCountry(lat, lo):
-    url = __HOST.format(lat, lo)
+def toCountry(keys):
+    if type(keys) in {list, tuple}:
+        url = __HOST_lo_la.format(keys[0], keys[1])
+    else:
+        url = __HOST_loc.format(keys.replace(" ", "+"))
     resp = requests.get(url, proxies=proxy_provider.get_proxy(), verify=False)
 
     if resp.status_code == 200:
         return resp.text
     if resp.status_code == 503:
-        # Inelegant way of dealing with the G captcha
         raise Exception('Error: {0} {1}'.format(resp.status_code, resp.reason))
     else:
         raise Exception('Error: {0} {1}'.format(resp.status_code, resp.reason))
@@ -21,13 +27,23 @@ def toCountry(lat, lo):
 
 if __name__ == '__main__':
     dir_result = os.path.join("loc")
-    with open("affiliation_lo_la_11-8.json", "r") as f:
+    with open("all_affi.json", "r") as f:
         js = json.load(f)
         for k, v in js.items():
             file_res = os.path.join(dir_result, k)
             if v['lo'] == v['la'] == 0:
-                continue
-            with open(file_res, "w", encoding="utf-8") as fw:
-                loc_info = toCountry(v['la'], v['lo'])
-                fw.write(loc_info)
-            print(k)
+                keys = v['name']
+            else:
+                keys = (v['la'], v['lo'])
+
+            have_try = 0
+            while have_try < __MAX_TRY:
+                try:
+                    have_try += 1
+                    loc_info = toCountry(keys)
+                    with open(file_res, "w", encoding="utf-8") as fw:
+                        fw.write(loc_info)
+                    break
+                except Exception as e:
+                    print(k, str(e))
+
